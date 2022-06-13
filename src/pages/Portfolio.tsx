45292832de6ec7cf2d8474ed2d00651bcdea8595
Bryson Taylor <BrysonTTaylor@gmail.com>
2022-06-13 14:36:00 -0700

    Limited portfolio menu to only technologies that are included in a project.

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import techBadges from '../resources/data/techBadges';
+import projects from '../resources/data/projects';
 import FilterButton from '../components/Buttons/FilterButton';
 import PortfolioList from '../components/PortfolioList/PortfolioList';
 import PageTitle from '../components/PageTitle/PageTitle';
@@ -10,20 +12,37 @@ type PortfolioProps = {
 
 const Portfolio:React.FC<PortfolioProps> = () => {
 
+  const limitTechBadgesToActive = () => {
+    let projectBadges: Array<string> = [];
+    
+    let badgeArrays = projects.map(proj => proj.badges);
+    projectBadges = badgeArrays.flat();
+    projectBadges = projectBadges.filter(b => b !== '');
+    projectBadges = [...new Set(projectBadges)];
+
+    
+    setActiveTechBadges(techBadges.filter(badge => projectBadges.includes(badge.text.toLowerCase())));
+  }
+
+  const [activeTechBadges, setActiveTechBadges] = useState(techBadges);
   const [filters, setFilters] = useState<Array<string>>([]);
-  
+
+  useEffect(() => {
+    limitTechBadgesToActive();
+  }, []);
+
   return (
     <div className="portfolio">
       <PageTitle text="Portfolio" />
       <div className="portfolio__project-list">
         <div className="portfolio__filter-buttons">
           {
-            techBadges.map(({text, logo}) => <FilterButton text={ text } logo={ logo } filters={ filters } setFilters={ setFilters } />)
+            activeTechBadges.map(({text, logo}) => <FilterButton key={ uuid() } text={ text } logo={ logo } filters={ filters } setFilters={ setFilters } />)
           }
         </div>
         <PortfolioList filters={ filters } setFilters={ setFilters } />
       </div>
     </div>
-  )
+  );
 }
 export default Portfolio;
